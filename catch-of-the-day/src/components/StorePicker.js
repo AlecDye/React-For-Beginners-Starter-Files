1b4be835747de1fa0a3aec23b57173edb471b9c4
Alec Dye <alecdye@gmail.com>
2020-03-09 10:44:57 -0700

    Adding form inputs to StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,22 @@
 import React from "react"
 
+// Landing page component: allows you to name the store
 class StorePicker extends React.Component {
     render() {
-        return <p>Now we're cooking</p>
+        // <React.Fragment> is the same as <> </>
+        return (
+            <>
+                <form className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                    <input
+                        type="text" required placeholder="Store Name"
+                    />
+                    <button type="submit">
+                        Visit Store ➡
+                    </button>
+                </form>
+            </>
+        )
     }
 }
 
